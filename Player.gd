f84511bd2562284143ddd3903b71a6682c82c8eb
Andrey Arehov <programmirovanie.io@gmail.com>
2020-07-24 11:25:04 +0300

    Оживил нашего персонажа

--- Player.gd
+++ Player.gd
@@ -1,16 +1,17 @@
 extends KinematicBody2D
 
+const SPEED = 100
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2(0, -1)
 
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		$Sprite.flip_h = true
+	else:
+		velocity.x = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	move_and_slide(velocity)
