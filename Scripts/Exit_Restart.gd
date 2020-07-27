c8d865edf0f4cf431ad821e7fff45d8b5a789906
Antony <43521351+MacAntony@users.noreply.github.com>
2020-07-27 14:42:25 +0300

    Функцыя урона
    End - кнопка вызова фнукции. Для теста

--- Scripts/Exit_Restart.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready():
-	$CanvasLayer2/Restart.connect('pressed',self, "btn_restart")
-
-
-func btn_restart():
-	get_tree().change_scene("res://test_scene.tscn")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,16 +4,18 @@ export var SPEED: int; # Скорость
 export var GRAVITY: int; # Скорость падения
 export var JUMP_POWER: int; # Скорость прыжка
 
-
-var lives = 3;
+const power = 70
 var is_fall = false;
-var is_fall_anim = false
+var is_anim = false
 const FLOOR = 	Vector2(0, -1)
 var velocity = Vector2()
+var is_damage = false
+
+var HP = 9;
 
 func _process(delta):
-	# TODO: Фикс, при сочетании других клавиш не работает прыжок
-	if Input.is_action_pressed("ui_right") && !is_fall_anim:
+	$GUI/CanvasLayer/NinePatchRect.texture.set_current_frame(HP / 3)
+	if Input.is_action_pressed("ui_right") && !is_anim && !is_damage:
 		velocity.x = SPEED
 		$AnimatedSprite.flip_h = false	
 		if is_on_floor(): # Если мы на земле, можем двигаться как обычно
@@ -21,54 +23,68 @@ func _process(delta):
 		else: 
 			$AnimatedSprite.play("jump")
 				
-	elif Input.is_action_pressed("ui_left") && !is_fall_anim:
+	elif Input.is_action_pressed("ui_left") && !is_anim && !is_damage:
 		velocity.x = -SPEED
 		$AnimatedSprite.flip_h = true
 		if is_on_floor(): # Если мы на земле, можем двигаться как обычно (2)
 			$AnimatedSprite.play("walk")
 		else:
 			$AnimatedSprite.play("jump")
-		
-	else: # Если просто стоим, то мы просто стоим =) 
+	
+	elif Input.is_action_pressed("ui_end"):
+		damage(1)
+	
+	elif !is_damage: # Если просто стоим, то мы просто стоим =) 
 		velocity.x = 0
-		if is_on_floor() && !is_fall_anim: 
+		if is_on_floor() && !is_anim: 
 			$AnimatedSprite.play("idle") 
-		
+	
+	if is_damage && is_on_floor():
+		is_damage = false
+	
 	if !is_on_floor() && velocity.y > JUMP_POWER:
 		$AnimatedSprite.play("fall")
 		is_fall = true
 		
 	if velocity.y == 0 && is_fall:
 		$AnimatedSprite.play("endFall")
-		is_fall_anim = true
+		is_anim = true
 		
-	if Input.is_action_pressed("ui_up") && is_on_floor() && !is_fall_anim:  # Если прыжок на месте делаем анимацыю прыжка
+	if Input.is_action_pressed("ui_up") && is_on_floor() && !is_anim:  # Если прыжок на месте делаем анимацыю прыжка
 		velocity.y = -JUMP_POWER
-		$AnimatedSprite.play("idleJump") # TODO: Фикс анимации
+		$AnimatedSprite.play("idleJump") 
 	
 	velocity.y +=  (GRAVITY * delta) # Гравитация каждый фиксированный кадр
 	velocity = move_and_slide(velocity, FLOOR) 
-	
-func add_live():
-	lives += 1
 
 func _on_AnimatedSprite_animation_finished():
-	if is_fall: 
-		is_fall = false
-		is_fall_anim = false
+	is_fall = false
+	is_anim = false
 	pass # Replace with function body.
 
-func damage():
-	$AnimatedSprite.play("Damage")
+func damage(var damage: int):
+	if !is_damage:
+		is_damage = true
+		is_anim = true
+		if $AnimatedSprite.flip_h:
+			velocity.y = -power
+			velocity.x = power
+		else:
+			velocity.y = -power
+			velocity.x = -power
+		$AnimatedSprite.play("damage")
+		HP -= damage
+		if HP <= 0 :
+			Main.scene('game_over')
+#		else:
+#			Main.scene(Main.level())
 
 func die():
 	$AnimatedSprite.play("Die")
-	lives -= 1
-	$GUI/CanvasLayer/NinePatchRect.texture.set_current_frame(lives)
-	if lives <= 0 :
+	HP -= 3;
+	if HP <= 0 :
 		Main.scene('game_over')
-#	else:
-#		Main.scene(Main.level())
 
 func _ready(): # Тестирование
+	damage(6)
 	die();
