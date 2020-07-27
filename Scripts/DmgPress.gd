5225f7648bb1ddb330f2e79064d58e0fd5bc5b58
Ilja <68640036+Grivamor@users.noreply.github.com>
2020-07-27 22:51:31 +0300

    Передал наработки пресса

--- Scripts/DmgPress.gd
+++ Scripts/DmgPress.gd
@@ -1,5 +1,16 @@
 extends CollisionShape2D
 
+var PressIn = false
+
 func _on_Hit_body_entered(Hit):
-	if "Player" in Hit.name:
+	
+	
+	
+	if ("Dynamic" in "Hit") == false:
+		PressIn = false
+	
+	if ("Dynamic" in "Hit") == true:
+		PressIn = true
+	
+	if "Player" in Hit.name and PressIn == true:
 		Hit.damage(1)
